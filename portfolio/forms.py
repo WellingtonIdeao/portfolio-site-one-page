c39309e11cd9a0db6fc8a416027cfe74416a3589
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-17 18:57:29 -0300

    created a contact form without widget_tweaks app. Styling via modelForm

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -3,6 +3,22 @@ from .models import Contact
 
 
 class ContactForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': "Enter your name"})
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': "name@example.com"})
+        self.fields['subject'].widget.attrs.update({'class': 'form-control', 'placeholder': "Enter your subject"})
+        self.fields['message'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': "Enter your message", 'style': "height: 10rem"}
+        )
+        self.label_suffix = ''
+
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'message']
+        labels = {
+            'name': 'Nome',
+            'subject': 'Assunto',
+            'message': 'Mensagem'
+        }
