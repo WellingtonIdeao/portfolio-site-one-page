c67ba237b35ad84febc71e19f72d5207a176d27c
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-16 22:53:41 -0300

    Added functional form in index.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import Index
 
 app_name = 'portfolio'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='portfolio/index_temp.html'), name='index'),
+    path('', Index.as_view(), name='index'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+
+class Index(CreateView):
+    form_class = ContactForm
+    template_name = 'portfolio/index.html'
+    success_url = '/thanks/'
